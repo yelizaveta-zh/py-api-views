7e6c1be1960303862643789343db9417e14b00d1
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-02-11 10:05:30 +0200

    add models

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,40 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    row = models.PositiveIntegerField()
+    seat_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name="movies"
+    )
 
     def __str__(self):
         return self.title
